2c7fa5e09c3645bf3443a080e5772cb5f2dcbad7
EvaChudia <eva.chudina@yandex.ru>
2020-03-19 22:57:55 +0300

    initial upload

---
+++ app.js
@@ -0,0 +1,44 @@
+let aboutWindow = document.querySelector('.about');
+let secondWindow = document.querySelector('.second-window');
+let close1Window = document.querySelector( '.close-1');
+let thirdWindow = document.querySelector('.third-window','third-window-text', 'third-window-img');
+let close2Window = document.querySelector( '.close-2');
+let fourthWindow = document.querySelector('.fourth-window','.fourth-window-text', '.fourth-window-img')
+let close3Window = document.querySelector( '.close-3');
+let fiveWindow = document.querySelector('.five-window','.five-window-text', '.five-window-img')
+let close4Window = document.querySelector( '.close-4');
+let sixWindow = document.querySelector('.six-window','.six-window-text', '.six-window-img')
+let close5Window = document.querySelector( '.close-5');
+let sevenWindow = document.querySelector('.seven-window','.seven-window-text', '.seven-window-img')
+let close6Window = document.querySelector( '.close-6');
+let eightWindow = document.querySelector('.eight-window','.eight-window-text', '.eight-window-img')
+let close7Window = document.querySelector( '.close-7');
+
+
+
+aboutWindow.addEventListener('click', function(){
+    secondWindow.style.setProperty('visibility', 'visible');
+});
+
+close1Window.addEventListener('click', function(){
+    thirdWindow.style.setProperty('visibility', 'visible');
+
+});
+close2Window.addEventListener('click', function(){
+    fourthWindow.style.setProperty('visibility', 'visible');
+});
+close3Window.addEventListener('click', function(){
+    fiveWindow.style.setProperty('visibility', 'visible');
+});
+close4Window.addEventListener('click', function(){
+    sixWindow.style.setProperty('visibility', 'visible');
+});
+close5Window.addEventListener('click', function(){
+    sevenWindow.style.setProperty('visibility', 'visible');
+});
+close6Window.addEventListener('click', function(){
+    eightWindow.style.setProperty('visibility', 'visible');
+});
+
+
+
